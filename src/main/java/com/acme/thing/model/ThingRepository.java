21ead20cfa4089cb67bfae77093a1776c650f951
Michael Turner <michael.turner@veeva.com>
2018-12-20 10:25:57 -0600

    change id type on repository to match change in entity

--- src/main/java/com/acme/thing/model/ThingRepository.java
+++ src/main/java/com/acme/thing/model/ThingRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ThingRepository extends PagingAndSortingRepository<Thing, Long> {
+public interface ThingRepository extends PagingAndSortingRepository<Thing, String> {
 }
